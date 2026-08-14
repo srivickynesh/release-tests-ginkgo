9f52a04e2da628c8613e243ff71cb3343232cedd
Sri Vignesh <sselvan@redhat.com>
2026-08-14 16:20:46 +0530

    fix vulnerabilities and remove vendor folder (#60)
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>

--- vendor/github.com/openshift-pipelines/pipelines-as-code/pkg/cli/prompt/prompt.go
+++
@@ -1,9 +0,0 @@
-package prompt
-
-import "github.com/AlecAivazis/survey/v2"
-
-// SurveyAskOne ask one question to be stubbed later.
-var SurveyAskOne func(survey.Prompt, any, ...survey.AskOpt) error = survey.AskOne
-
-// SurveyAsk ask questions to be stubbed later.
-var SurveyAsk func([]*survey.Question, any, ...survey.AskOpt) error = survey.Ask
--- vendor/github.com/openshift-pipelines/pipelines-as-code/pkg/params/info/vcsconfig.go
+++
@@ -1,8 +0,0 @@
-package info
-
-type ProviderConfig struct {
-	TaskStatusTMPL string
-	APIURL         string
-	Name           string
-	SkipEmoji      bool
-}
