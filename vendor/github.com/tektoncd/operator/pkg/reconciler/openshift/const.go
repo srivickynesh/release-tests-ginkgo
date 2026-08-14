e60ea07a7fce93b8847cb430718a10d5eed85dfb
Sri Vignesh <sselvan@redhat.com>
2026-08-14 13:11:04 +0530

    fix vulnerabilities and remove vendor folder
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>

--- vendor/github.com/tektoncd/operator/pkg/reconciler/openshift/const.go
+++
@@ -1,9 +0,0 @@
-package openshift
-
-const (
-	KueueNameSpace                  = "openshift-kueue-operator"
-	OperandOpenShiftPipelinesAddons = "openshift-pipelines-addons"
-	OperandOpenShiftPipelineAsCode  = "openshift-pipeline-as-code"
-	// NamespaceSCCAnnotation is used to set SCC for a given namespace
-	NamespaceSCCAnnotation = "operator.tekton.dev/scc"
-)
